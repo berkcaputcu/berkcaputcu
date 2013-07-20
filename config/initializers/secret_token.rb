4f56c256e25a258dce8b43224ca29602496fe446
Berk Caputcu <berkcaputcu@gmail.com>
2013-07-20 12:51:16 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Berkcaputcu::Application.config.secret_token = 'ebc64cfd6b6abb7196ef37a247cf64f0e49b783aaf247121cefbb8420d3a9a01ff4f0bca35963dd7dbe8a5943b0998c97d98a56b77fac7acf4b02d0bc24e0b8a'
